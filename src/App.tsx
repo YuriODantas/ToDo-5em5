7bbc286277393f0a01a76e80e2d51221dd9ea099
YuriODantas <79267155+YuriODantas@users.noreply.github.com>
2021-10-18 22:32:00 -0300

    Add array Itens ToDo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import * as C from './App.styles'
+import { ListItem } from './components/ListItem';
+import { Item } from './types/item'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false },
+    { id: 1, name: 'Comprar banana', done: true }
+  ]);
+
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Adicionar nova tarefas */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
     );
